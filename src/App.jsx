3ac57019af0bebd25227dbdcf175d9f562e7a19a
unknown <mr_vlasov@mail.ru>
2022-06-15 09:35:06 +0300

    решена проблема с фильтрацие

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,38 @@ import { useEffect, useState } from 'react';
 
 function App() {
 	const [todos, setTodos] = useState([
-		{
-			 "id": "2022-06-14T19:09:20.299Z",
-			 "text": "ss",
-			 "completed": false
-		},
-		{
-			 "id": "2022-06-14T19:09:21.275Z",
-			 "text": "ddd",
-			 "completed": false
-		},
-		{
-			 "id": "2022-06-14T19:16:21.360Z",
-			 "text": "sss",
-			 "completed": false
-		}
+		// {
+		// 	 "id": "2022-06-14T19:09:20.299Z",
+		// 	 "text": "ss",
+		// 	 "completed": false
+		// },
+		// {
+		// 	 "id": "2022-06-14T19:09:21.275Z",
+		// 	 "text": "ddd",
+		// 	 "completed": false
+		// },
+		// {
+		// 	 "id": "2022-06-14T19:16:21.360Z",
+		// 	 "text": "sss",
+		// 	 "completed": false
+		// }
 	])
-	const [allTodos, setAllTodos] = useState([])
+	const [filtered, setFiltered] = useState([])
 
 	const [text, setText] = useState('')
+
+	useEffect(() => {
+		setFiltered(todos)
+	}, [todos])
+	
+
 
 	const addTodo = () => {
 		if (text.trim().length) {
 			setTodos([...todos,
 			{
 				id: new Date().toISOString(),
-				text: text,
+				text,
 				completed: false,
 			}
 			])
@@ -36,9 +42,6 @@ function App() {
 			setText('')
 		}
 	}
-
-
-	
 
 
 	function handlePressEnter(e) {
@@ -65,47 +68,15 @@ function App() {
 		}
 	})
 
-	function activeTodo() {
-		setAllTodos(todos)
-		setTodos(todos.filter(el => {
-			if (el.completed === false) {
-				return el
-			}
-		}))
-	}
-
-	function complitedTodo() {
-		if (todos.find(item => item.completed === false)) {
-			setAllTodos(todos)
-			setTodos(todos.filter(el => {
-				if (el.completed === true) {
-					return el
-				}
-			}))
+	function todoFilter(status) {
+		if (status === 'all') {
+			setFiltered(todos)
+		} else {
+			let newTodo = todos.filter(item => item.completed === status)
+			setFiltered(newTodo)
 		}
-
 	}
 
-	function AllToto() {
-		setTodos(allTodos)
-	}
-
-
-	// if () {
-	// 	console.log('sad');
-	// }
-
-	// console.log(arr.find(item => {
-	// 	return item.completed
-	// }));
-	// console.log(arr.find(item => item.complited === true));
-	//console.log(arr.includes(arr));
-
-	//arr.includes(item => item.completed === true)
-
-	//console.log(todos);
-	let newTodo = [...todos].filter(item => item.completed === false)
-	console.log(newTodo);
 	return (
 		<div className="App">
 			<input className='inpt'
@@ -115,7 +86,7 @@ function App() {
 			/>
 			<ul>
 				{
-					todos.map((todo) => (
+					filtered.map((todo) => (
 						<li key={todo.id}>
 							<input type="checkbox"
 								checked={todo.completed}
@@ -128,12 +99,38 @@ function App() {
 				}
 			</ul>
 			<span>{itemsLeft.length + " "}items left</span>
-			<button onClick={AllToto} >All</button>
-			<button onClick={activeTodo}>Active</button>
-			<button onClick={complitedTodo}>Completed</button>
-			<button>Clear completed</button>
+			<button onClick={(e) => todoFilter('all')} >All</button>
+			<button onClick={(e) => todoFilter(false)}>Active</button>
+			<button onClick={(e) => todoFilter(true)}>Completed</button>
+			<button onClick={(e) => todoFilter('Clear')}>Clear completed</button>
 		</div>
 	);
 }
 
 export default App;
+
+
+// function activeTodo() {
+// 	setAllTodos(todos)
+// 	setTodos(todos.filter(el => {
+// 		if (el.completed === false) {
+// 			return el
+// 		}
+// 	}))
+// }
+
+// function complitedTodo() {
+// 	if (todos.find(item => item.completed === false)) {
+// 		setAllTodos(todos)
+// 		setTodos(todos.filter(el => {
+// 			if (el.completed === true) {
+// 				return el
+// 			}
+// 		}))
+// 	}
+
+// }
+
+// function AllToto() {
+// 	setTodos(allTodos)
+// }
